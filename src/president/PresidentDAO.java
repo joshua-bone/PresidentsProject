5b6c8769c49c19378f3ac5cd6fdefef22924d3b9
chrisbuttaro <chris.buttaro@gmail.com>
2016-11-05 09:24:52 -0600

    updated interface again

--- src/president/PresidentDAO.java
+++ src/president/PresidentDAO.java
@@ -1,13 +1,23 @@
 package president;
 
 public interface PresidentDAO {
-	
-	void readFile(); 
-	
-	String getName();
-	
+
+	public void readFile();
+
+	public void getName();
+
+	public void getTerm();
+
+	public void getParty();
+
+	public void fact();
+
+	public void mgURL();
+
 	public void incremenetIndex();
+
 	public void decrementIndex();
+
 	public void setIndex(int i);
 
 }
