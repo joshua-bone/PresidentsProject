e53fedc6f0a24e78f631b9b3f76d8aae4378c2da
joshua-bone <joshua.bone@hotmail.com>
2016-11-05 09:52:42 -0600

    update servlet to use DAO methods

--- src/president/PresidentsServlet.java
+++ src/president/PresidentsServlet.java
@@ -35,22 +35,31 @@ public class PresidentsServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		Integer index = (Integer) session.getAttribute("index");
-		if (index == null) {
-			index = 1;
-			session.setAttribute("index", index);
+		
+		PresidentDAO dao = (PresidentDAO) session.getAttribute("dao");
+		if (dao == null){
+			dao = new PresDAO();
+			dao.readFile(); //debug: move to dao constructor
+			dao.setIndex(1); //debut: move to dao constructor
+			session.setAttribute("dao", dao);
 		}
-		ServletContext context = getServletContext();
-		ArrayList<President> presidents = (ArrayList<President>) context.getAttribute("presidents");
+		
 		String direction = req.getParameter("navigate");
 		switch (direction) {
 		case "forward":
-			index ++;
+			dao.incremenetIndex();;
 			break;
 		case "back":
-			index --;
+			dao.decrementIndex();
 			break;
+		case "getTerm":
+			int i = 1;
+			try{ 
+				i = Integer.parseInt(req.getParameter("term"));
+			} catch (NumberFormatException nfe){}
+			dao.setIndex(i);
 		}
+		
 		RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
 		dispatcher.forward(req,  resp);
 	}
